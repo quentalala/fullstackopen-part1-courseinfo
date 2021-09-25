489d74ed81200e9c04be4f415db904663ce300ad
Quentin Chin <q.chin@hotmail.com>
2021-09-24 21:01:39 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React from "react";
+import Header from "./components/Header";
+import Content from "./components/Content";
+import Total from "./components/Total";
+
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content
+        part1={course.parts[0].name}
+        part2={course.parts[1].name}
+        part3={course.parts[2].name}
+        exercises1={course.parts[0].exercises}
+        exercises2={course.parts[1].exercises}
+        exercises3={course.parts[2].exercises}
+      />
+      <Total
+        exercises1={course.parts[0].exercises}
+        exercises2={course.parts[1].exercises}
+        exercises3={course.parts[2].exercises}
+      />
+    </div>
+  );
+};
+
+export default App;
